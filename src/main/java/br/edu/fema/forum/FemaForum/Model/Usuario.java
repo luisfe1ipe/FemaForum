b97055118e5486afe30085e3b2aeb752f764dedc
Luis Felipe <luisfelipe.pxl@gmail.com>
2024-02-06 10:03:43 -0300

    feat: criando model Usuario

---
+++ src/main/java/br/edu/fema/forum/FemaForum/Model/Usuario.java
@@ -0,0 +1,18 @@
+package br.edu.fema.forum.FemaForum.Model;
+
+public class Usuario {
+    private Long id;
+    private String nome;
+    private String email;
+    private String senha;
+
+
+    @Override
+    public int hashCode()
+    {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+}
